e90da116bc2029d58cac60015121643ea9e481f5
Germano José Sorte Mualevangue <germanosorte9@gmail.com>
2026-04-06 23:18:55 +0100

    feat: adicionar URL Vercel no CORS

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL
+console.log('Conectado em:', SERVER_URL)
 
 const socket = io(SERVER_URL, {
     autoConnetct: false
--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,7 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://where2-go-kappa.vercel.app/"
+  "https://where2-go-kappa.vercel.app"
 ]
 
 app.use(cors({
